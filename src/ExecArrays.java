f73bc1643317c16c67aae4724118bcb8beef1b9a
Vinícius Santos <vinichius@gmail.com>
2021-03-18 19:40:42 -0300

    Inicio arrays bi

--- src/ExecArrays.java
+++ src/ExecArrays.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class ExecArrays {
 
@@ -6,10 +8,10 @@ public class ExecArrays {
 		//MetodosArrays testarMetodosArray = new MetodosArrays(); Se usar metodos static não precisa instanciar pra chamar o metodo
 		
 		int[] arrayDeInt = new int[10];
-		arrayDeInt[0]= 1;
-		arrayDeInt[1]= 2;
-		arrayDeInt[2]= 3;
-		arrayDeInt[3]= 4;
+		arrayDeInt[0]= 2;
+		arrayDeInt[1]= 1;
+		arrayDeInt[2]= 4;
+		arrayDeInt[3]= 3;
 		arrayDeInt[4]= 5;
 		arrayDeInt[5]= 6;
 		arrayDeInt[6]= 7;
@@ -20,7 +22,7 @@ public class ExecArrays {
 		int[] segunoArrayDeInt = new int[3];
 		segunoArrayDeInt[0] = 11;
 		segunoArrayDeInt[1] = 12;
-		segunoArrayDeInt[2] = 155;
+		segunoArrayDeInt[2] = -2;
 		
 		//array de objetos não basta ser instanciada, também tem que ser popupada por ojetos do tipo declarado
 		String[] arrayDeString = new String[5];
@@ -36,6 +38,13 @@ public class ExecArrays {
 		arrayBoolean[2] = true;
 		arrayBoolean[3] = false;
 		
+		char arrayChar[] = new char[5];
+		arrayChar[0] = '0';
+		arrayChar[1] = '0';
+		arrayChar[2] = '7';
+		arrayChar[3] = '.';
+		arrayChar[4] = '.';
+		
 		//Exe1
 		//MetodosArrays.pressContrario(arrayDeInt);
 		
@@ -49,10 +58,40 @@ public class ExecArrays {
 		//MetodosArrays.zeroUm(arrayBoolean);
 
 		//Exe6
-		MetodosArrays.terceiroArray(arrayDeInt, segunoArrayDeInt);
+		//MetodosArrays.terceiroArray(arrayDeInt, segunoArrayDeInt);
 		
 		//Exe7
+		//MetodosArrays.arrayMenosMais(arrayDeInt, segunoArrayDeInt);
+		//MetodosArrays.imprimeChar(arrayChar);
 		
+		//ARRAY BIDIMENSIONAL
+
+		//Exe1
+		/*
+		int[][] arrayBidim;
+		arrayBidim = MetodosArrays.arrayBidim();
+		for(int linha =0; linha < arrayBidim.length; linha++) {
+			for(int coluna =0; coluna < arrayBidim[linha].length; coluna++) {
+				System.out.print(arrayBidim[linha][coluna]);
+			}
+			System.out.println();
+		}
+		*/
+		//Exe2
+		//MetodosArrays.arrayBidimensionalMaior();
+		
+		//Exe3
+		int[][] arrayBidim2;
+		arrayBidim2 = MetodosArrays.arrayBidimensional1ao5();
+		for(int i =0; i < arrayBidim2.length; i++) {
+			for(int j =0; j < arrayBidim2[i].length; j++) {
+				System.out.print(arrayBidim2[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		//Exe
 		
 
 	}
--- src/MetodosArrays.java
+++ src/MetodosArrays.java
@@ -89,6 +89,91 @@ public class MetodosArrays {
 	
 	//Exe7 Faça um método q recebe dois arrays de inteiros, e retorna um terceiro array que possui todos os elementos
 	//negativos (dos dois arrays) na primeira parte e todos os elementos positivos (inclusive o zero) na segunda parte (dos dois arrays).
+	public static void arrayMenosMais(int[] primeiroArray, int[] segundoArray) {
+		int[] arrayTerceiro = new int[primeiroArray.length + segundoArray.length];
+		int auxIndiceArray = 0;
+		
+		for(int i = 0; i < primeiroArray.length; i++) {
+			arrayTerceiro[auxIndiceArray] = primeiroArray[i];
+			auxIndiceArray++;
+		}
+		for(int i = 0; i < segundoArray.length; i++) {
+			arrayTerceiro[auxIndiceArray] = segundoArray[i];
+			auxIndiceArray++;
+		}
+		/*
+		//metodo para trocar os números, se for maior que o próximo, faz a troca
+		for(int i = 0; i < arrayTerceiro.length; i++) {
+			if(i+1 == arrayTerceiro.length) {
+				break;
+			}
+			if(arrayTerceiro[i] > arrayTerceiro[i+1]) {
+				int[]aux = new int[1];
+				aux[0] = arrayTerceiro[i];
+				arrayTerceiro[i] = arrayTerceiro[i+1];
+				arrayTerceiro[i+1] = aux[0];
+				//System.out.println(arrayTerceiro[i]);
+			}
+		}
+		*/
+		
+		for(int i = 0; i < arrayTerceiro.length; i++) {
+			System.out.println("Valor na posição "+i+" é: "+arrayTerceiro[i]);
+		}
+	}
+	
+	//Exe8 Faça um método chamado q recebe um array de char e imprime na tela (na mesma linha) todos os elementos do array
+	public static void imprimeChar(char[] recebeChar) {
+		for(int i = 0; i < recebeChar.length; i++) {
+			System.out.print(recebeChar[i]);
+		}
+	}
+	
+	
+	//Exe1 Crie um método que retorna oarray bidimensional de inteiros
+	public static int[][] arrayBidim(){
+		int[][] arrayBi = new int[3][10];
+		for(int linha = 0; linha < 3; linha++) {
+			for(int coluna = 0; coluna < 10; coluna++) {
+				arrayBi[linha][coluna] = coluna; //recebe UM VALOR o posição do eixo xy
+			}
+		}
+		return arrayBi;
+	}
+	
+	
+	//Exe2 Crie um método que retorna oarray bidimensional maior que o anterior de int
+	public static void arrayBidimensionalMaior() {
+		int [][] arrayBidim = new int [5][10];
+		
+		for(int linha = 0; linha < arrayBidim.length; linha++) {
+			for(int coluna = 0; coluna < arrayBidim[linha].length; coluna++) {
+				System.out.print(arrayBidim[linha][coluna] = coluna * coluna);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	//Exe3
+	public static int[][] arrayBidimensional1ao5 (){
+		int [][] arrayBidim = new int[6][6];
+		int aux = 0;
+		for(int linha = 0; linha < arrayBidim.length; linha++) {
+			aux = 0;
+			for(int coluna = 0; coluna < arrayBidim[linha].length; coluna++) {
+				arrayBidim[linha][coluna] = aux;
+				aux++;
+			}
+		}
+		return arrayBidim;
+	}
+	
+	//Exe2
+	
+	
+	
+	
 	
 	
 }
