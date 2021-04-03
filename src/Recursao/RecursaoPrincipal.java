38498b99886e80b5b14e5e10702eb308ce9f077a
Vinícius Santos <vinichius@gmail.com>
2021-04-03 18:56:27 -0300

    Concluido a Exe 2

--- src/Recursao/RecursaoPrincipal.java
+++ src/Recursao/RecursaoPrincipal.java
@@ -4,13 +4,15 @@ public class RecursaoPrincipal {
 
 	public static void main(String[] args) {
 		
+		//Exe 1
 		int[] arrayDeInt = new int[10];
 		for(int i = 0; i < arrayDeInt.length; i++ ) {
 			arrayDeInt[i] = i + 1;
 		}
-		RecursaoPrincipal.imprimeArrayAoContrario(arrayDeInt, arrayDeInt.length - 1);
+		//RecursaoPrincipal.imprimeArrayAoContrario(arrayDeInt, arrayDeInt.length - 1);
 		
-		double[][] arrayBidiDouble = new double [5][5];
+		//Exe 2
+		double[][] arrayBidiDouble = new double [3][3];
 		for(int i = 0; i < arrayBidiDouble.length; i++) {
 			//double d = i;
 			//arrayBidiDouble[i] = 1.0;
@@ -18,10 +20,15 @@ public class RecursaoPrincipal {
 				arrayBidiDouble[i][j] = j+1;
 			}
 		}
+		System.out.println(RecursaoPrincipal.somaBidimensional(arrayBidiDouble));
+		//RecursaoPrincipal.somaBidimensional(arrayBidiDouble);
+		
+		//Exe 3
+		
 
 	}
 	
-	//método recursivo que imprime um array recebido por parâmetro de trás para frente
+	//Exe 1 método recursivo que imprime um array recebido por parâmetro de trás para frente
 	public static void imprimeArrayAoContrario(int[] array, int indice) {
 		if(indice >= 0) {
 			System.out.println(array[indice]);
@@ -30,7 +37,23 @@ public class RecursaoPrincipal {
 		}
 	}
 	
-	//método  recursivo  que  recebe  um  array  bidimensional  de  double  e retorna a soma dos elementos deste array
+	//Exe 2 método  recursivo  que  recebe  um  array  bidimensional  de  double  e retorna a soma dos elementos deste array
+	public static double somaBidimensional(double[][] arrayBidi) {
+		return somaBidimensional(arrayBidi, 0, 0);//vou te retornar esse método que é um metodo que retorna um double
+	}
+	private static double somaBidimensional(double[][] arrayBidi, int linha, int coluna) {
+		if(linha >= arrayBidi.length) {
+			return 0;//quando não tiver mais linhas eu retorno zero	
+		}
+		if(coluna >= arrayBidi[linha].length) {
+			return somaBidimensional(arrayBidi, linha + 1, 0);//quando terminar as colunas eu vou para a próxima linha
+		}
+		
+		return arrayBidi[linha][coluna] + somaBidimensional(arrayBidi, linha, coluna +1);//quando terminar linhas e colunas e retorno a soma de traz pra frente
+	}
+	
+	//Exe 3 rie  um  método  chamado  encontraPosicaoMenorValor.
+	//encontrar  a  posição  do  menor  valor  de  um  array  de  inteiros  recebido  por parâmetro
 	
 	
 	
